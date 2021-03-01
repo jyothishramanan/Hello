24e0a706f2e9cd7d7eee8f9235b7bb1f86f59f22
unknown <jyothishramanan@gmail.com>
2021-03-01 20:17:15 +0530

    My first Commit

---
+++ ProductWebApp/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductWebApp.Models;
+using ProductWebApp.Services;
+
+namespace ProductWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _producService;
+ 
+        public ProductController(ProductService productService)
+        {
+            _producService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> Get()
+        {
+           return _producService.Get();
+           
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public ActionResult<Product> Get(string id)
+        {
+            var prod = _producService.Get(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+
+            return prod;
+        }
+    }
+}
---
+++ ProductWebApp/Models/ProductDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApp.Models
+{
+    public class ProductDatabaseSettings: IProductDatabaseSettings
+    {
+        public string ProductCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface IProductDatabaseSettings
+    {
+        public string ProductCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
